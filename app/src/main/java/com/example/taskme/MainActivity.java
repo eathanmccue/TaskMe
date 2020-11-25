3a304b3bbabf9b278121dd80f2b31a7cbe8832cd
eathanmccue <eathan.mccue@gmail.com>
2020-11-24 17:31:09 -0800

    newEvent button opens the activity_create_event.xml added save button to activity_create_event.xml which sends back to main added red/orange gradient; gradient_background_red.xml added EditText variables to CreateEvent.java **need to send EditText values back to main to be displayed**

--- app/src/main/java/com/example/taskme/MainActivity.java
+++ app/src/main/java/com/example/taskme/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.taskme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+    }
+
+    public void newEvent(View view) {
+        Intent intent = new Intent(this, CreateEvent.class);
+        startActivity(intent);
     }
 }
