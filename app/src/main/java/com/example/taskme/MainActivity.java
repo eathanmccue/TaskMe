da392870b6212d22767448211e32c893dbcec78f
Amandeep <amandeep.tuli18@gmail.com>
2020-11-27 03:27:03 +0530

    Created add event button test.
    - Created list view.
    - Add event  button for now populates the list.

--- app/src/main/java/com/example/taskme/MainActivity.java
+++ app/src/main/java/com/example/taskme/MainActivity.java
@@ -3,13 +3,36 @@ package com.example.taskme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> tasksList = new ArrayList<String>();
+    ArrayAdapter<String> tasksListAdapter;
 
+    ListView tasksListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        tasksListView = (ListView) findViewById(R.id.tasksListView);
+
+        tasksListAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                tasksList);
+
+        tasksListView.setAdapter(tasksListAdapter);
+
+    }
+
+    public void addEvent(View view) {
+        tasksList.add("New item!");
+        tasksListAdapter.notifyDataSetChanged();
     }
 }
