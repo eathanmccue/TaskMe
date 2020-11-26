3834768f82ed7f3ed96e3062df0e4d57bfdc086a
Amandeep <amandeep.tuli18@gmail.com>
2020-11-27 03:38:02 +0530

    Fixed errors after merging.

--- app/src/main/java/com/example/taskme/MainActivity.java
+++ app/src/main/java/com/example/taskme/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
