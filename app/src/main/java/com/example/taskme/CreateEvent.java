c9f91791df70d922b6b20cdbf3754740292467d9
Amandeep <amandeep.tuli18@gmail.com>
2020-11-27 07:42:25 +0530

    Added logic for inserting row to DB.

--- app/src/main/java/com/example/taskme/CreateEvent.java
+++ app/src/main/java/com/example/taskme/CreateEvent.java
@@ -8,24 +8,39 @@ import android.widget.EditText;
 
 public class CreateEvent extends AppCompatActivity {
 
-    public EditText name;
-    public EditText description;
-    public EditText date;
-    public EditText time;
+    public String name, description, date, time;
+    TasksDbHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_create_event);
+
+        // the db helper object
+        helper = TasksDbHelper.getsInstance(this);
     }
 
     public void saveButton(View view) {
-        name = findViewById(R.id.createName);
-        description = findViewById(R.id.createDescription);
-        date = findViewById(R.id.createDate);
-        time = findViewById(R.id.createTime);
+        name = findViewById(R.id.createName).toString();
+        description = findViewById(R.id.createDescription).toString();
+        date = findViewById(R.id.createDate).toString();
+        time = findViewById(R.id.createTime).toString();
 
+        NewTaskDetails newTask = new NewTaskDetails();
+        newTask.name = name;
+        newTask.desc = description;
+        newTask.date = date;
+        newTask.time = time;
+
+        newTask.is_goal = 1;
+        newTask.is_important = 1;
+        newTask.is_reminder = 1;
+        newTask.is_repeat = 1;
+
+        helper.addTask(newTask);
+
+        setResult(2);
         finish();
     }
 }
