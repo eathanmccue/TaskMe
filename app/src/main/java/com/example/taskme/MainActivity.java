327b3466a30e9ba83e899bdc7ba2f023732db3ed
Amandeep <amandeep.tuli18@gmail.com>
2020-11-27 07:42:55 +0530

    Change startActivity to startativity for result

--- app/src/main/java/com/example/taskme/MainActivity.java
+++ app/src/main/java/com/example/taskme/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.taskme;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -13,16 +15,16 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     ArrayList<String> tasksList = new ArrayList<String>();
     ArrayAdapter<String> tasksListAdapter;
-
     ListView tasksListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
-        tasksListView = (ListView) findViewById(R.id.tasksListView);
 
+        tasksListView = (ListView) findViewById(R.id.tasksListView);
         tasksListAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1,
                 tasksList);
@@ -33,12 +35,16 @@ public class MainActivity extends AppCompatActivity {
     // Real event
     public void newEvent(View view) {
         Intent intent = new Intent(this, CreateEvent.class);
-        startActivity(intent);
+        startActivityForResult(intent, 2);
     }
 
-    // Testing event, will be removed later.
-    public void addEvent(View view) {
-        tasksList.add("New item!");
-        tasksListAdapter.notifyDataSetChanged();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == 2){
+            tasksList.add("New item!");
+            tasksListAdapter.notifyDataSetChanged();
+        }
     }
 }
