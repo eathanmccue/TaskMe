a573628870359d301c43366eb5f0a9accc83fa23
Amandeep <amandeep.tuli18@gmail.com>
2020-11-30 01:17:07 +0530

    Tasks list logic added.
    - Changes from list view to recycler
    - Created logic to store data in recyler view
    - Created function to inflate the recyler view.

--- app/src/main/java/com/example/taskme/MainActivity.java
+++ app/src/main/java/com/example/taskme/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.taskme;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,11 +20,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    ArrayList<String> tasksList = new ArrayList<String>();
-    ArrayAdapter<String> tasksListAdapter;
-    ListView tasksListView;
 
     TasksDbHelper helper;
+    ArrayList<String> task_name_list, task_description_list, date_list, time_list;
+    TasksListAdapter tasksListAdapter;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,22 +32,23 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
-
-        tasksListView = (ListView) findViewById(R.id.tasksListView);
-        tasksListAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,
-                tasksList);
-
-        tasksListView.setAdapter(tasksListAdapter);
-
         // the db helper object
         helper = new TasksDbHelper(MainActivity.this);
+        task_name_list = new ArrayList<>();
+        task_description_list = new ArrayList<>();
+        date_list = new ArrayList<>();
+        time_list = new ArrayList<>();
+
+        recyclerView = findViewById(R.id.tasksListView);
+
+        storeDataInArray();
 
     }
+
     // Real event
     public void newEvent(View view) {
         Intent intent = new Intent(this, CreateEvent.class);
-        startActivityForResult(intent, 2);
+        startActivityForResult(intent, 1);
     }
 
     @Override
@@ -51,18 +56,34 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if(resultCode == 2){
-            displayTasks();
+            storeDataInArray();
         }
     }
 
-    public void displayTasks(){
+    public void storeDataInArray(){
         Cursor data = helper.getTasks();
+
+        task_name_list.clear();
+        task_description_list.clear();
+        date_list.clear();
+        time_list.clear();
 
         if (data.getCount() > 0){
             while(data.moveToNext()){
-                Log.d("cursordata", data.getString(1));
+                // create an array of tasks objects
+                task_name_list.add(data.getString(1));
+                task_description_list.add(data.getString(2));
+                date_list.add(data.getString(3));
+                time_list.add(data.getString(4));
             }
+            inflateRecyclerView();
         }
 
+    }
+    private void inflateRecyclerView() {
+        tasksListAdapter = new TasksListAdapter(this, task_name_list, task_description_list, date_list, time_list);
+        recyclerView.setAdapter(tasksListAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.bringToFront();
     }
 }
