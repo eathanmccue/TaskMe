4813f0a49624a43b400f979c2b93ffd4b6ab10b7
Amandeep <amandeep.tuli18@gmail.com>
2020-11-29 06:17:08 +0530

    Get correct data after creating event.

--- app/src/main/java/com/example/taskme/CreateEvent.java
+++ app/src/main/java/com/example/taskme/CreateEvent.java
@@ -3,12 +3,13 @@ package com.example.taskme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 public class CreateEvent extends AppCompatActivity {
 
-    public String name, description, date, time;
+    public EditText name, description, date, time;
     TasksDbHelper helper;
 
     @Override
@@ -18,20 +19,20 @@ public class CreateEvent extends AppCompatActivity {
         setContentView(R.layout.activity_create_event);
 
         // the db helper object
-        helper = TasksDbHelper.getsInstance(this);
+        helper = new TasksDbHelper(CreateEvent.this);
     }
 
     public void saveButton(View view) {
-        name = findViewById(R.id.createName).toString();
-        description = findViewById(R.id.createDescription).toString();
-        date = findViewById(R.id.createDate).toString();
-        time = findViewById(R.id.createTime).toString();
+        name = findViewById(R.id.createName);
+        description = findViewById(R.id.createDescription);
+        date = findViewById(R.id.createDate);
+        time = findViewById(R.id.createTime);
 
         NewTaskDetails newTask = new NewTaskDetails();
-        newTask.name = name;
-        newTask.desc = description;
-        newTask.date = date;
-        newTask.time = time;
+        newTask.name = name.getText().toString();
+        newTask.desc = description.getText().toString();
+        newTask.date = date.getText().toString();
+        newTask.time = time.getText().toString();
 
         newTask.is_goal = 1;
         newTask.is_important = 1;
