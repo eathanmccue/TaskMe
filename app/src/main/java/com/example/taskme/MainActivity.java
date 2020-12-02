c24acb529ee11fdd6d0895f328127eef612c055e
Amandeep <amandeep.tuli18@gmail.com>
2020-12-03 00:02:55 +0530

    Added logic to take content from the calendar!

--- app/src/main/java/com/example/taskme/MainActivity.java
+++ app/src/main/java/com/example/taskme/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.taskme;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -9,9 +10,13 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CalendarView;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,14 +24,25 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<String> tasks_ids, task_name_list, task_description_list, date_list, time_list;
     TasksListAdapter tasksListAdapter;
     RecyclerView recyclerView;
-
+    TextView monthTxt, yearTxt, sDay, sMonth, sYear;
     TextView no_tasks_found;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+
+        Calendar cal = Calendar.getInstance();
+        int yearString = cal.get(Calendar.YEAR);
+        String monthString = cal.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault());
+
+        int selectedDay = cal.get(Calendar.DAY_OF_MONTH);
+        int selectedMonth = cal.get(Calendar.MONTH);
+        int selectedYear = cal.get(Calendar.YEAR);
+
 
         // the db helper object
         helper = new TasksDbHelper(MainActivity.this);
@@ -39,16 +55,45 @@ public class MainActivity extends AppCompatActivity {
         time_list = new ArrayList<>();
 
         recyclerView = findViewById(R.id.tasksListView);
+        yearTxt = findViewById(R.id.get_year);
+        monthTxt = findViewById(R.id.get_month);
+        sDay = findViewById(R.id.selected_day);
+        sMonth = findViewById(R.id.selected_month);
+        sYear = findViewById(R.id.selected_year);
         no_tasks_found = findViewById(R.id.no_tasks_found);
+        CalendarView calendarView = findViewById(R.id.calendarView);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                sDay.setText(String.valueOf(dayOfMonth));
+                sMonth.setText(String.valueOf(month));
+                sYear.setText(String.valueOf(year));
+            }
+        });
+
         no_tasks_found.setVisibility(View.GONE);
 
-        storeDataInArray();
+        monthTxt.setText(monthString);
+        yearTxt.setText(String.valueOf(yearString));
 
+        sDay.setText(String.valueOf(selectedDay));
+        sMonth.setText(String.valueOf(selectedMonth));
+        sYear.setText(String.valueOf(selectedYear));
+
+        storeDataInArray();
     }
 
     // Click event to add new task
     public void newEvent(View view) {
+        String sday = sDay.getText().toString();
+        String smonth = sMonth.getText().toString();
+        String syear = sYear.getText().toString();
+
         Intent intent = new Intent(this, CreateEvent.class);
+        intent.putExtra("sday",sday);
+        intent.putExtra("smonth",smonth);
+        intent.putExtra("syear",syear);
         startActivityForResult(intent, 1);
     }
 
